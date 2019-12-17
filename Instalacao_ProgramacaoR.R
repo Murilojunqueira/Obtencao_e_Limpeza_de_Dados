b219fbd46bd4cc8072f66da94ebc8a7c1896f0d2
Murilojunqueira <m.junqueira@yahoo.com.br>
2019-12-17 14:07:42 -0300

    Arquivo de instalação da lição

--- Instalacao_ProgramacaoR.R
+++ Instalacao_ProgramacaoR.R
@@ -3,8 +3,8 @@
 if(!"swirl" %in% rownames(installed.packages())) {install.packages("swirl")}
 library(swirl)
 select_language(language = "portuguese")
-try(uninstall_course("Programacao em R"), silent = TRUE)
-install_course_github("Murilojunqueira", "Programacao_em_R")
+try(uninstall_course("Obtencao_e_Limpeza_de_Dados"), silent = TRUE)
+install_course_github("Murilojunqueira", "Obtencao_e_Limpeza_de_Dados")
 
 
 swirl()
